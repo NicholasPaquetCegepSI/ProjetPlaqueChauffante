89030d0bfe510caa3192395addca4a809249cb99
Nicholas Paquet-Rowsell <nicholas.paquet.rowsell@cegepsi.ca>
2024-02-13 13:54:47 -0500

    Ajout du capteur de temperature et d'une fonction de delai

--- ControlPlaque/controlPlaque/controlPlaque.ino
+++ ControlPlaque/controlPlaque/controlPlaque.ino
@@ -1,12 +1,18 @@
-#define LED_BLEUE A0
-#define LED_JAUNE A1
-#define LED_VERTE A2
+#define LED_BLEUE 0
+#define LED_JAUNE 1
+#define LED_VERTE 2
+
+#define LM35 A0
 
 void setup() {
+  Serial.begin(9600);
+
   // Déclaration des LEDs
   pinMode(LED_BLEUE, OUTPUT);
   pinMode(LED_JAUNE, OUTPUT);
   pinMode(LED_VERTE, OUTPUT);
+
+  pinMode(LM35, INPUT);
 }
 
 void loop() {
@@ -14,4 +20,33 @@ void loop() {
   digitalWrite(LED_BLEUE, HIGH);
   digitalWrite(LED_JAUNE, HIGH);
   digitalWrite(LED_VERTE, HIGH);
+
+  analogReadResolution(12);
+  getTemperature(true);
+}
+
+void getTemperature(bool print) {
+  // Lecture de la valeur analogique du capteur
+  int valeurDuCapteur = analogRead(LM35);
+  // Calcul de conversion pour une résolution de 12 bits
+  double temperatureCelcius = (double)valeurDuCapteur * (3.3 / 40.96);
+
+  if (print) {
+    Serial.print("Température: ");
+    Serial.println(temperatureCelcius);
+  }
+}
+
+// Fonction de temporisation, retourne faux tant que la durée passée en argument ne s'est pas écoulée.
+bool tempo(int duree) {
+  // La fonction millis retourne le temps écoulé depuis le début de l'éxécution du Arduino.
+  unsigned long millisPresent = millis();
+
+  // Si le temps présent a dépassé le temps précédent (temps depuis la dernière temporisation) + la durée,
+  // on retourne vrai car la temporisation est terminé et on met à jour le temps précédent.
+  if (millisPresent - millisPrecedent >= +duree) {
+    millisPrecedent = millisPresent;
+    return true;
+  }
+  return false;
 }
